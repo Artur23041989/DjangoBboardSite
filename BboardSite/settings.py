39d26302791695045a5a5bcf3a2625e22291fd6c
Artur23041989 <danh1989@mail.ru>
2025-05-17 22:56:11 +0300

    new comit

--- BboardSite/settings.py
+++ BboardSite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import AUTH_USER_MODEL, LOGOUT_REDIRECT_URL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = 'bboard.AdvUser'
 # Application definition
 
 INSTALLED_APPS = [
@@ -122,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'bboard:index'
--- bboard/views.py
+++ bboard/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
-# Create your views here.
+from django.http import HttpResponse, Http404
+from django.template import TemplateDoesNotExist
+from django.template.loader import get_template
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
     return render(request, 'bboard/index.html')
+
+def other_page(request, page):
+    try:
+        template = get_template('bboard/' + page + '.html')
+    except TemplateDoesNotExist:
+        raise Http404
+    return HttpResponse(template.render(request=request))
+
+class BBLoginView(LoginView):
+    template_name = 'bboard/login.html'
+
+class BBLogoutView(LogoutView):
+    pass
+
+@login_required
+def profile(request):
+    return render(request, 'bboard/profile.html')
+
