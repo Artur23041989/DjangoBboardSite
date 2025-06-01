776393d912fe6cb05e622e9f38aaab82ff5ad8fc
Artur23041989 <danh1989@mail.ru>
2025-06-01 13:25:06 +0300

    new comit

--- bboard/urls.py
+++ bboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (index, other_page, BBLoginView, profile, BBLogoutView,
                     ProfileEditView, PasswordEditView, RegisterView,
                     RegisterDoneView, ProfileDeleteView, rubric_bbs, bb_detail,
-                    profile_bb_detail, profile_bb_add)
+                    profile_bb_detail, profile_bb_add, profile_bb_edit, profile_bb_delete)
 
 
 app_name = 'bboard'
@@ -13,6 +13,8 @@ urlpatterns = [
     path('account/password/edit', PasswordEditView.as_view(), name='password_edit'),
     path('accounts/profile/delete', ProfileDeleteView.as_view(), name='profile_delete'),
     path('accounts/profile/edit', ProfileEditView.as_view(), name='profile_edit'),
+    path('accounts/profile/edit/<int:pk>/', profile_bb_edit, name='profile_bb_edit'),
+    path('accounts/profile/delete/<int:pk>/', profile_bb_delete, name='profile_bb_delete'),
     path('accounts/profile/add',profile_bb_add, name='profile_bb_add'),
     path('accounts/profile/<int:pk>/', profile_bb_detail, name='profile_bb_detail'),
     path('accounts/profile/', profile, name='profile'),
